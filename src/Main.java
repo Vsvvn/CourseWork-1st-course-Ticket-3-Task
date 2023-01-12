10df74ea7eb602c9b0637359095a03b09c17996d
Vsvvn <Vladsun@yandex.ru>
2023-01-12 20:25:26 +0300

    Задача решена.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String string = "Слово";
+        int divideStringNumber = 2;
+        char[] word = string.toCharArray();
+        int resultDivided = word.length / divideStringNumber;
+        for (int firstHalfString = 0; firstHalfString < resultDivided; firstHalfString++) {
+            System.out.print(word[firstHalfString]);
+        }
+
+        System.out.println();
+
+        int secondHalfString;
+
+        if (word.length % 2 == 0) {
+            for (int i = 0; i < resultDivided; i++) {
+                secondHalfString = resultDivided + i;
+                System.out.print(word[secondHalfString]);
+            }
+
+        } else {
+            for (int i = 0; i <= resultDivided; i++) {
+                secondHalfString = resultDivided + i;
+                System.out.print(word[secondHalfString]);
+            }
+        }
     }
 }
